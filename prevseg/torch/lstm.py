63debd6c1aae64186354ca82868f3dad80b7e525
apra <dd.apra93@gmail.com>
2020-09-07 20:54:00 -0400

    SCI: Preliminary finding is that stacked LSTMs dont match human fMRI

--- prevseg/torch/lstm.py
+++ prevseg/torch/lstm.py
@@ -1,5 +1,5 @@
 import math
-import torch as th
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor as T
@@ -29,14 +29,18 @@ class LSTM(nn.Module):
         
         self.i2h = nn.Linear(input_size, 4 * hidden_size, bias=bias)
         self.h2h = nn.Linear(hidden_size, 4 * hidden_size, bias=bias)
-        
+
+        if torch.__version__ == '1.4.0' and torch.cuda.is_available():
+            self.i2h = self.i2h.cuda()
+            self.h2h = self.h2h.cuda()
+            
         self.reset_parameters()
         assert(dropout_method.lower() in ['pytorch', 'gal', 'moon', 'semeniuta'])
         self.dropout_method = dropout_method
 
     def sample_mask(self):
         keep = 1.0 - self.dropout
-        self.mask = V(th.bernoulli(T(1, self.hidden_size).fill_(keep)))
+        self.mask = V(torch.bernoulli(T(1, self.hidden_size).fill_(keep)))
 
     def reset_parameters(self):
         std = 1.0 / math.sqrt(self.hidden_size)
@@ -64,20 +68,20 @@ class LSTM(nn.Module):
         if do_dropout and self.dropout_method == 'semeniuta':
             g_t = F.dropout(g_t, p=self.dropout, training=self.training)
 
-        c_t = th.mul(c, f_t) + th.mul(i_t, g_t)
+        c_t = torch.mul(c, f_t) + torch.mul(i_t, g_t)
 
         if do_dropout and self.dropout_method == 'moon':
-            c_t.data.set_(th.mul(c_t, self.mask).data)
+            c_t.data.set_(torch.mul(c_t, self.mask).data)
             c_t.data *= 1.0 / (1.0 - self.dropout)
 
-        h_t = th.mul(o_t, c_t.tanh())
+        h_t = torch.mul(o_t, c_t.tanh())
 
         # Reshape for compatibility
         if do_dropout:
             if self.dropout_method == 'pytorch':
                 F.dropout(h_t, p=self.dropout, training=self.training, inplace=True)
             if self.dropout_method == 'gal':
-                h_t.data.set_(th.mul(h_t, self.mask).data)
+                h_t.data.set_(torch.mul(h_t, self.mask).data)
                 h_t.data *= 1.0 / (1.0 - self.dropout)
 
         h_t = h_t.view(1, h_t.size(0), -1)
